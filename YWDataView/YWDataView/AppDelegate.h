9c7eb016e3e830a5766520fd2e83f8c02c7f04f8
yellowwing <923133828@qq.com>
2019-12-11 14:58:13 +0800

    1

---
+++ YWDataView/YWDataView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YWDataView
+//
+//  Created by yellow on 2019/12/11.
+//  Copyright © 2019 YW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ YWDataView/YWDataView/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  YWDataView
+//
+//  Created by yellow on 2019/12/11.
+//  Copyright © 2019 YW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
